db1a376eeb786a1d226c8304fda0176e7da15339
powellstreet <cropleyave@gmail.com>
2026-01-02 18:55:32 +0900

    chore: setup swagger documentation

--- apps/api/src/book/book.controller.ts
+++ apps/api/src/book/book.controller.ts
@@ -1,11 +1,16 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { BookService } from './book.service';
+import { ApiOperation, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Book')
 @Controller('book')
 export class BookController {
   constructor(private readonly bookService: BookService) {}
 
   @Get('search')
+  @ApiOperation({ summary: 'Search books via Google Books API' })
+  @ApiQuery({ name: 'q', required: true, description: 'Book title or author' })
+  @ApiResponse({ status: 200, description: 'List of books found.' })
   async search(@Query('q') query: string) {
     return this.bookService.searchBooks(query);
   }
